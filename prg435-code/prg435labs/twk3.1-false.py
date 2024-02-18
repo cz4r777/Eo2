202f628fb533da4ec5e62994f524676113d35351
cz4r777 <110761805+cz4r777@users.noreply.github.com>
2024-02-18 22:02:31 +0800

    Add files via upload

---
+++ prg435-code/prg435labs/twk3.1-false.py
@@ -0,0 +1,18 @@
+###################################################
+#Lab 3.1 PRG435
+#Conrad Brookes
+#14 Feb 2024
+# GNU
+# Press the green button in the gutter to run the script.
+####################################################
+int = 'a'
+a = '33'
+int = 'b'
+b = '33'
+
+if b == a:
+    print("there equal")
+else:
+    print("there not")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ prg435-code/prg435labs/twk3.2l.py
@@ -0,0 +1,2 @@
+if 1 == 1 and 2 == 10:
+    print("Matched with OR!")
